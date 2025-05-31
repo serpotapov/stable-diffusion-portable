86a55a486720894e42b59c8ab434fa8444e1ebe3
Sergey <potapovss2002@gmail.com>
2025-05-31 19:37:55 +0300

    Update

--- webui-user-first-run.cmd
+++ webui-user-first-run.cmd
@@ -2,10 +2,22 @@
 del README.md
 echo Unpacking Git and Python... & mkdir tmp & start /wait git_python.part01.exe & del git_python.part01.exe & del git_python.part*.rar
 
-set path=git\cmd
+set appdata=tmp
+set userprofile=tmp
+set temp=tmp
+set path=git\cmd;python
+
 git init .
 git remote add origin https://github.com/AUTOMATIC1111/stable-diffusion-webui.git
 git pull origin master
+
+python -m venv venv
+call venv\scripts\activate.bat
+
+python -m pip install --upgrade pip
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu128
+
+call venv\scripts\deactivate.bat
 
 del webui-user.bat
 ren webui-user1.bat webui-user.bat
